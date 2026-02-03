062fe5e0a27c8e49037a431a8d758fa0e6a7e956
Oksana Vakuliak <oksanabagrova19@gmail.com>
2026-02-03 21:30:36 +0100

    feat: add Header component with active link styling

---
+++ components/Header/Header.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Link from "next/link";
+import css from "./Header.module.css";
+import { usePathname } from "next/navigation";
+
+export default function Header() {
+  const pathname = usePathname();
+
+  return (
+    <header className={css.header}>
+      <Link href={"/"} className={css.logo}>
+        <svg width="104" height="16" className={css.logoIcon}>
+          <use href="/sprite.svg#logo"></use>
+        </svg>
+      </Link>
+
+      <ul className={css.navigationList}>
+        <li>
+          <Link
+            href={"/"}
+            className={`${css.linkItem} ${pathname === "/" ? css.active : ""}`}
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={"/catalog"}
+            className={`${css.linkItem} ${pathname.startsWith("/catalog") ? css.active : ""}`}
+          >
+            Catalog
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+}
